db663343266befe50dfa78aa9731b29c0e681787
Iceberry-qdd <qddwork@outlook.com>
2020-08-19 16:09:39 +0800

    update day12

---
+++ day12/1.py
@@ -0,0 +1,18 @@
+import re
+
+
+def main():
+    username = input('请输入用户名：')
+    qq = input('请输入qq号：')
+    m1 = re.match(r'^[0-9a-zA-Z_]{6,20}$', username)
+    if not m1:
+        print('请输入有效的用户名！')
+    m2 = re.match(r'^[1-9]\d{4,11}$', qq)
+    if not m2:
+        print('请输入有效的QQ号.')
+    if m1 and m2:
+        print('信息有效！')
+
+
+if __name__ == "__main__":
+    main()
